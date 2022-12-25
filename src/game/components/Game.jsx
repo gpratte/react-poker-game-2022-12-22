2f8fa97a7b27737486eecd04433f021dff84fbbc
Gil Pratte <gil.pratte@gmail.com>
2022-12-24 19:28:30 -0600

    manage all notification in a list

--- src/game/components/Game.jsx
+++ src/game/components/Game.jsx
@@ -3,7 +3,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import {Badge, Nav, NavDropdown, Toast} from "react-bootstrap";
+import {Badge, Button, ListGroup, Nav, NavDropdown, Offcanvas, Toast} from "react-bootstrap";
 import React, {useEffect, useState} from "react";
 import {isEmpty} from 'lodash';
 
@@ -35,8 +35,10 @@ function TopNavBar(props) {
               }
               {props.notificationCount > 0 &&
                 <>
-                  <i className="nav-home fa-solid fa-bell"></i>
-                  <Badge bg={"warning"}>{props.notificationCount}</Badge>
+                  <span style={{cursor: "pointer"}} onClick={() => props.showNotifications()}>
+                    <i className="nav-home fa-solid fa-bell"></i>
+                    <Badge bg={"warning"}>{props.notificationCount}</Badge>
+                  </span>
                 </>
               }
             </Navbar.Text>
@@ -49,7 +51,7 @@ function TopNavBar(props) {
 
 function ErrorNotification(props) {
   return (
-    <Toast show={!isEmpty(props.notification)} onClose={() => props.clearNotification(props.notification.id)}>
+    <Toast show={!isEmpty(props.notification)} onClose={() => props.deleteNotification(props.notification.id)}>
       <Toast.Header>
         <strong className="me-auto">{props.notification.type}</strong>
       </Toast.Header>
@@ -58,17 +60,70 @@ function ErrorNotification(props) {
   )
 }
 
+function Notifications(props) {
+  let notifications;
+  if (props.notifications && props.notifications.length > 0) {
+    // Show the newest (the last in the list) first
+    notifications = [...props.notifications].reverse();
+  } else {
+    notifications = []
+  }
+  return (
+    <Offcanvas show={props.show} onHide={props.hide}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Notifications</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body key={Date.now().valueOf()}>
+        <>
+          <Button variant={"primary"}
+                  disabled={notifications.length === 0}
+                  onClick={() => props.deleteAllNotifications()}>
+            Clear all
+          </Button>
+          <ListGroup>
+            {notifications.map(notification => {
+              return <ListGroup.Item key={notification.id}>
+                <Button variant={"link"} onClick={() => props.deleteNotification(notification.id)}>
+                  Delete
+                </Button>
+                {
+                  notification.message
+                }
+              </ListGroup.Item>
+            })}
+          </ListGroup>
+        </>
+      </Offcanvas.Body>
+    </Offcanvas>
+  )
+}
+
 function Game() {
   // state for showing/hiding the toast
   const [notification, setNotification] = useState({});
   const [notifications, setNotifications] = useState([]);
   const [notificationCount, setNotificationCount] = useState(0)
+  const [showNotifications, setShowNotifications] = useState(false);
 
-  const clearNotification = (id) => {
+  const deleteNotification = (id) => {
     const newNotifications = notifications.filter(notification => notification.id !== id)
     setNotification({});
     setNotifications(newNotifications);
     setNotificationCount(newNotifications.length);
+  }
+
+  const deleteAllNotifications = () => {
+    setNotification({});
+    setNotifications([]);
+    setNotificationCount(0);
+  }
+
+  const showNotificationsPanel = () => {
+    setNotification({});
+    setShowNotifications(true);
+  }
+  const hideNotificationsPanel = () => {
+    setShowNotifications(false);
   }
 
   // toggle the toast
@@ -79,24 +134,33 @@ function Game() {
         type: 'Error',
         message: 'uh oh ' + Date.now()
       }
-      setNotification(notify);
       const newNotifications = [...notifications];
       newNotifications.push(notify);
       setNotifications(newNotifications)
       setNotificationCount(newNotifications.length);
-    }, 7000);
+      if (!showNotifications) {
+        setNotification(notify);
+      }
+    }, 2500);
     return () => clearInterval(interval);
   })
 
 
   return (
     <>
-      <TopNavBar notificationCount={notificationCount}/>
+      <TopNavBar notificationCount={notificationCount} showNotifications={showNotificationsPanel}/>
       <Container className="main-view">
         <Row className="justify-content-center text-center">
           <Col>
-            <ErrorNotification notification={notification} clearNotification={clearNotification}/>
+            <ErrorNotification notification={notification}
+                               deleteNotification={deleteNotification}
+            />
             Main page
+            <Notifications show={showNotifications}
+                           hide={hideNotificationsPanel}
+                           deleteNotification={deleteNotification}
+                           deleteAllNotifications={deleteAllNotifications}
+                           notifications={notifications}/>
           </Col>
         </Row>
       </Container>
