40d14a552d1d9d56098850381780ecf9a58d08b7
Gil Pratte <gil.pratte@gmail.com>
2022-12-26 12:25:53 -0600

    show spinner when loading

--- src/game/components/Game.jsx
+++ src/game/components/Game.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Details from "./Details";
 import useGame from "../hooks/useGame";
+import {Spinner} from "react-bootstrap";
 
 function Game(props) {
 
@@ -10,7 +11,13 @@ function Game(props) {
   } = useGame(props.newNotification);
 
   if (isLoading) {
-    return <div>Loading...</div>
+    return (
+      <div>
+        <Spinner animation="border">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
+      </div>
+    )
   }
 
   return (
